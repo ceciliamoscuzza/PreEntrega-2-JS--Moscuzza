1a973d2f3d813e3078ddda8db8d5e60daf78d21b
Cecilia Moscuzza <cecimoscu@gmail.com>
2023-07-23 14:55:35 -0300

    genere imagenes

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,72 @@
 const carrito = [];
 
 const productos = [
-  { codigo: "a", nombre: "Cata Nacional", precio: 5000 },
-  { codigo: "b", nombre: "Cata Importado", precio: 8000 },
-  { codigo: "c", nombre: "Cata De autor", precio: 12000 },
-  { codigo: "d", nombre: "Cata Mix Premium", precio: 16000 },
-  { codigo: "e", nombre: "Whisky Habermas", precio: 16000 },
-  { codigo: "f", nombre: "Whisky Butler", precio: 7990 },
-  { codigo: "g", nombre: "Whisky Latour", precio: 14000 },
-  { codigo: "h", nombre: "Whisky Zizek", precio: 8562 },
-  { codigo: "i", nombre: "Whisky Nussbaum", precio: 18000 },
-  { codigo: "j", nombre: "Whisky Taylor", precio: 32000 },
-  { codigo: "k", nombre: "Whisky Badiou", precio: 45000 },
+  {
+    codigo: "a",
+    imagen: "/imagenes/cataNac.png",
+    nombre: "Cata Nacional",
+    precio: 5000,
+  },
+  {
+    codigo: "b",
+    imagen: "/imagenes/cataImp.png",
+    nombre: "Cata Importado",
+    precio: 8000,
+  },
+  {
+    codigo: "c",
+    imagen: "/imagenes/cataAutor.png",
+    nombre: "Cata De autor",
+    precio: 12000,
+  },
+  {
+    codigo: "d",
+    imagen: "/imagenes/cataMix.png",
+    nombre: "Cata Mix Premium",
+    precio: 16000,
+  },
+  {
+    codigo: "e",
+    imagen: "/imagenes/w1.png",
+    nombre: "Whisky Habermas",
+    precio: 16000,
+  },
+  {
+    codigo: "f",
+    imagen: "/imagenes/w2.png",
+    nombre: "Whisky Butler",
+    precio: 7990,
+  },
+  {
+    codigo: "g",
+    imagen: "/imagenes/w3.png",
+    nombre: "Whisky Latour",
+    precio: 14000,
+  },
+  {
+    codigo: "h",
+    imagen: "/imagenes/w4.png",
+    nombre: "Whisky Zizek",
+    precio: 8562,
+  },
+  {
+    codigo: "i",
+    imagen: "/imagenes/w5.png",
+    nombre: "Whisky Nussbaum",
+    precio: 18000,
+  },
+  {
+    codigo: "j",
+    imagen: "/imagenes/w6.png",
+    nombre: "Whisky Taylor",
+    precio: 32000,
+  },
+  {
+    codigo: "k",
+    imagen: "/imagenes/w7.png",
+    nombre: "Whisky Badiou",
+    precio: 45000,
+  },
 ];
 
 function buscarProductos(codigo) {
